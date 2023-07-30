701c3f653a1bb9d8f61a28bf55982229750809f5
moon <moon951205@naver.com>
2023-07-30 21:31:18 +0900

    gcp storage img upload2

--- googlestorage_IMG_save.py
+++ googlestorage_IMG_save.py
@@ -16,15 +16,15 @@ def upload_image_url_to_gcs(bucket_name, url, destination_blob_name):
         blob.upload_from_file(f, content_type='image/jpeg')
 
 #mongoDB와 연결
-client = MongoClient('mongodb://final:123@34.22.93.125:27017/')
+client = MongoClient('mongodb://[계정아이디]:[비밀번호]@[몽고디비IP]/')
 db = client['final']
 collection = db['movies']
 
 #연결된 도큐먼트들에서 필요한 정보들 수집 후 로컬에 이미지 저장없이 스토리지에 저장
 for i in collection.find().limit(10): # find()로 해야지 for문을 사용가능 10개만 뽑아봄
     url=i['poster_image_url']
-    bucket_name = 'moochu'  # Google Cloud Storage 버킷 이름
+    bucket_name = '버킷이름'  # Google Cloud Storage 버킷 이름
     destination_name=f'{str(i["_id"])}.jpg'# 이거를 사진이름으로 해야한다 
     
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ="/Users/gangsickmun/Desktop/파이널 aws/river-dynamo-393506-f834b1182f86.json" #키파일 위치
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ="키파일 경로"
     upload_image_url_to_gcs(bucket_name, url, destination_name)
